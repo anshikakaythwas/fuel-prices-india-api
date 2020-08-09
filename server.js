0ff504cdbb97e6b3f937f326d24f03b98295830c
Anshika Kaythwas <anshikakaythwas@gmail.com>
2020-08-09 17:26:39 +0530

    Port config

--- server.js
+++ server.js
@@ -293,6 +293,6 @@ app.get("/dev/allStates", function (req, res) {
   });
 });
 
-app.listen("8081");
+app.listen(process.env.PORT || 8081);
 console.log("Magic happens on port 8081");
 exports = module.exports = app;
